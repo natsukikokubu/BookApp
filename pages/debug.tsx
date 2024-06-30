237b1d47ee499e4e018d1e54a1da6dc3230dbfff
natsukikokubu <natsukikokubu3@gmail.com>
2024-06-30 20:22:16 +0900

    debug api呼び出し

--- pages/debug.tsx
+++ pages/debug.tsx
@@ -1,10 +1,31 @@
-import { supabase } from "@/utils/supabase";
-import { useEffect, useState } from "react";
+import { GetServerSideProps } from "next";
 
-export default function Debug() {
+type ServerProps = {
+  name: string;
+};
+
+export const getServerSideProps: GetServerSideProps<ServerProps> = async ({
+  req,
+}) => {
+  const host = req.headers.host || "localhost:3000";
+  const protocol = /^localhost/.test(host) ? "http" : "https";
+  console.log(req.headers);
+  const data = await fetch(`${protocol}://${host}/api/hello`).then((data) =>
+    data.json()
+  );
+
+  return {
+    props: data,
+  };
+};
+
+type Props = ServerProps;
+
+export default function Debug(props: Props) {
   return (
     <div>
       <h1>This is debug page</h1>
+      <span>My name is {props.name}</span>
     </div>
   );
 }
