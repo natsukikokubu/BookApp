94db0c1a68df522128cec3b7d3165105ff51ff82
natsukikokubu <natsukikokubu3@gmail.com>
2024-06-30 18:54:16 +0900

    修正

---
+++ pages/debug.tsx
@@ -0,0 +1,10 @@
+import { supabase } from "@/utils/supabase";
+import { useEffect, useState } from "react";
+
+export default function Debug() {
+  return (
+    <div>
+      <h1>This is debug page</h1>
+    </div>
+  );
+}
