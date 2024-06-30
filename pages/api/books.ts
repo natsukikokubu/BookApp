8f83da5e9a98341e59873ca37067eda9797b9a0a
natsukikokubu <natsukikokubu3@gmail.com>
2024-06-30 21:02:40 +0900

    add books api

---
+++ pages/api/books.ts
@@ -0,0 +1,17 @@
+import { supabase } from "../../utils/supabase";
+import type { NextApiRequest, NextApiResponse } from "next";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method === "GET") {
+    const data = await supabase.from("books").select("*");
+    console.log(data);
+
+    res.status(200).json({ data });
+  } else {
+    res.setHeader("Allow", ["GET"]);
+    res.status(405).end(`Method ${req.method} Not Allowed`);
+  }
+}
